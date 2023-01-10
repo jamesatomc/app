fa9c61cea8eebfef2ae91aa272f77e56bf18c594
jamesatomc <jiraphonwansai2002@gmail.com>
2023-01-10 21:57:27 +0700

    v0.0.1

---
+++ navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div>
+        <header className="flex justify-between items-center">
+        <a className="flex-1">
+
+        </a>
+        <ul></ul>
+        <span className='flex-1 text-right'>
+            <button>1</button>
+        </span>
+        </header> 
+    </div>
+  )
+}
